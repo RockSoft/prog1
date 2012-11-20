13bba9b68cee869639e29409b84f013e1fc0d0fe
Peter <peterleigh@(null).(none)>
2012-11-20 11:24:36 +0000

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Peter on 20/11/2012.
+//  Copyright (c) 2012 Rock Software Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
